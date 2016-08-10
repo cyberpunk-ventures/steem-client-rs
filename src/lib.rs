45e62f98caee2f5193a5c7eeb3331912925e6e8a
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-08-10 19:01:56 +0300

    basic jsonrpc request/response functionality is working, test passing

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate rustc_serialize;
 use hyper::*;
 use std::io::Read;
 use rustc_serialize::{Decodable, Encodable, json};
+use rustc_serialize::json::Json;
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct JsonRpcStruct {
@@ -12,7 +13,6 @@ struct JsonRpcStruct {
     params: (String, String, Vec<String>),
     id: i64,
 }
-
 
 pub fn call(params: (String, String, Vec<String>)) -> String {
     let client = Client::new();
@@ -35,13 +35,17 @@ pub fn call(params: (String, String, Vec<String>)) -> String {
     s
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn basic_json_rpc_call_works() {
-        let rpc_call = ("database_api".to_string(), "get_dynamic_global_properties", vec![]);
-        assert_eq!("", call(rpc_call));
+        let plugin = "database_api".to_string();
+        let method = "get_dynamic_global_properties".to_string();
+        let rpc_call = (plugin, method, vec![]);
+        let result_str = call(rpc_call);
+        assert!(result_str.contains("head_block_number"));
     }
 }
