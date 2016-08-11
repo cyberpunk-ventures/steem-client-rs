767caf12fa85ca09e30d01b2ac1d15ab41813658
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-08-11 20:21:13 +0300

    switched to an more* idiomatic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,42 @@
 extern crate hyper;
-extern crate rustc_serialize;
-#[macro_use]
-extern crate json;
+extern crate serde_json;
+extern crate env_logger;
 
 use hyper::*;
+use serde_json::Map;
 use std::io::Read;
-// use rustc_serialize::json::Json;
+use serde_json::builder::{ArrayBuilder, ObjectBuilder};
 
+pub fn call(api: String,
+            api_method: String,
+            args: Vec<String>)
+            -> Map<String, serde_json::value::Value> {
 
-pub fn call(api: String, api_method: String, args: Vec<String>) -> json::JsonValue {
+    let params = ArrayBuilder::new()
+        .push(api)
+        .push(api_method)
+        .push(args)
+        .build();
+
+    let value = ObjectBuilder::new()
+        .insert("jsonrpc".to_string(), "2.0")
+        .insert("method".to_string(), "call")
+        .insert("id".to_string(), "1")
+        .insert("params".to_string(), params)
+        .build();
+
     let client = Client::new();
 
-    let data = object!{
-        "jsonrpc" => "2.0",
-        "method" => "call",
-        "id" => 1,
-        "params" => array![api, api_method, args]
-    };
-
-    let json = data.dump();
     let mut res = client.post("http://node.steem.ws/rpc")
-        .body(&json)
+        .body(&serde_json::to_string(&value).unwrap())
         .send()
         .unwrap();
 
     let mut s = String::new();
     res.read_to_string(&mut s).unwrap();
-    let obj = json::parse(&s).unwrap();
-    obj
+    let json: Map<String, serde_json::value::Value> = serde_json::from_str(&s).unwrap();
+
+    json
 }
 
 
@@ -37,12 +46,12 @@ mod tests {
 
     #[test]
     fn basic_json_rpc_call_works() {
+
         let api = "database_api".to_string();
         let api_method = "get_dynamic_global_properties".to_string();
         let args = vec![];
-        let result_str = call(api, api_method, args);
-        let head_block_number = result_str["result"]["head_block_number"].as_fixed_point_u64(0);
-        println!("{:?}", head_block_number);
-        assert!(head_block_number.unwrap() > 3990000);
+        let result_map = call(api, api_method, args);
+
+        assert!(result_map.contains_key("id"));
     }
 }
